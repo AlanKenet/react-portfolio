75ad00be0d2fc7235c9c398d390352c13ed424d4
Alan Kenet <alankenet@gmail.com>
2024-09-10 16:07:45 -0700

    Changes in ContentViewer componet, related to ContentSection changes.

--- src/components/contentViewer/ContentViewer.jsx
+++ src/components/contentViewer/ContentViewer.jsx
@@ -5,21 +5,23 @@ import { About } from '@/pages/about/About'
 import { Experience } from '@/pages/experience/Experience'
 import { Contact } from '@/pages/contact/Contact'
 
+import './styles/ContentViewer.css'
+
 const actualPage = ['home', 'about', 'experience', 'contact']
 
 export function ContentViewer () {
   return (
     <main className='content-viewer'>
-      <ContentSection classes={`${actualPage[0]}-section`}>
+      <ContentSection section={actualPage[0]}>
         <Home />
       </ContentSection>
-      <ContentSection classes={`${actualPage[1]}-section`}>
+      <ContentSection section={actualPage[1]}>
         <About />
       </ContentSection>
-      <ContentSection classes={`${actualPage[2]}-section`}>
+      <ContentSection section={actualPage[2]}>
         <Experience />
       </ContentSection>
-      <ContentSection classes={`${actualPage[3]}-section`}>
+      <ContentSection section={actualPage[3]}>
         <Contact />
       </ContentSection>
     </main>
