12ad0bd8e4ce7a2ccee75f35b6ef914f00df2a73
Alan Kenet <alankenet@gmail.com>
2024-09-18 12:38:32 -0700

    General fix. New layouts sizes.

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from '@/App'
 
 import '@/styles/index.css'
 import '@/styles/utils.css'
+import '@/styles/animations.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
